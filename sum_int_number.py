ba90838e82bf7f31fe59a993be5f2dba57b48c1c
tabachukmaryana <xoxokimber0@gmail.com>
2024-02-10 19:04:39 +0200

    Resolve task using sum(), for loop, and try...except block

--- sum_int_number.py
+++ sum_int_number.py
@@ -1,5 +1,9 @@
 def sum_int_number(number):
-    return sum(range(number + 1))
+    try:
+        sum_result = sum(range(number + 1))
+    except TypeError:
+        sum_result = 0
+    return sum_result
 
 if __name__ == "__main__":
     print(sum_int_number(10))
