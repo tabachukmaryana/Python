2e6582e3f7db912f6250d7e470d1631c62f5e231
tabachukmaryana <xoxokimber0@gmail.com>
2024-02-10 18:04:52 +0200

    Resolve task using while loop

---
+++ sum_int_number.py
@@ -0,0 +1,8 @@
+def sum_int_number(number):
+    sum_result = 0
+    for num in range(number + 1):
+        sum_result += num
+    return sum_result
+
+if __name__ == "__main__":
+    print(sum_int_number(10))
