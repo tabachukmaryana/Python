bbeba51bd596ef8b19e2bf9ede17aca5537be728
tabachukmaryana <xoxokimber0@gmail.com>
2024-02-10 18:58:35 +0200

    Resolve task using sum() function

--- sum_int_number.py
+++ sum_int_number.py
@@ -1,9 +1,5 @@
-
 def sum_int_number(number):
-    sum_result = 0
-    for num in range(number + 1):
-        sum_result += num
-    return sum_result
+    return sum(range(number + 1))
 
 if __name__ == "__main__":
     print(sum_int_number(10))
